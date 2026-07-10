a679ff597261d92d56677804cd4e7e567e497288
hoangnm-ndm <hoangnm.ndm@gmail.com>
2026-07-10 21:10:26 +0700

    Day 05: BTTH

---
+++ Day05/ex04.js
@@ -0,0 +1,12 @@
+// export / import
+
+import { products } from "./data.js";
+
+console.log(products);
+
+/**
+ * * 1. Lấy danh sách sản phẩm mới với mỗi sản phẩm chỉ còn cần tên, giá, đánh giá, danh mục, thương hiệu, mô tả.
+ * * 2. Từ mảng ở bài 1, hãy lấy ra mảng mới chỉ chứa các sản phẩm thuộc danh mục beauty và có giá dưới 100$
+ * * 3. Từ mảng ban đầu, lần này sắp xếp theo giá tăng dần.
+ * * 4. Tìm sản phẩm có mã SKU (Stock Keeping Unit) là "FRA-DOL-DOL-009"
+ */
